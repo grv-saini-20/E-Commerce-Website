afa2654d0ec2102046efda6db0767266361e5854
grv-saini-20 <gouravsaini9797@gmail.com>
2024-09-06 16:00:21 +0530

    fix: deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/E-Commerce-Website/',
+  base: process.env.NODE_ENV === 'production' ? '/E-Commerce-Website/' : '/'
 })

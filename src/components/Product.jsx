c2eb16c1563d90f8c20a38f85605298d70463092
grv-saini-20 <gouravsaini9797@gmail.com>
2024-10-13 11:57:52 +0530

    fix: format

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from 'react'
+import React,{useState, useEffect} from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Card from "react-bootstrap/Card";
 import  Button  from 'react-bootstrap/Button';
